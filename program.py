4f517a45b0c75fa769af92fd95c7f224af962e70
badFarmer <guy@strategicmfg.com>
2019-01-02 19:52:29 -0800

    Incremental Commit

--- program.py
+++ program.py
@@ -6,8 +6,8 @@ print('              HELLO WORLD')
 print('--------------------------------------------')
 print()
 
-the_number = random.randint(0,100)
-
+the_number = random.randint(0, 100)
+name = input ('What is your name?')
 #print (the_number, type(the_number))
 #print (guess, type(guess))
 #print (guess_text, type(guess_text))
@@ -18,11 +18,11 @@ while guess != the_number:
     guess_text = input('Guess a number between 0 and 100: ')
     guess = int(guess_text)
     if guess < the_number:
-        print('too low, sucka.')
+        print('{0} is too low, sucka. I mean {1}.'.format(guess, name))
     elif guess > the_number:
-        print('too damn high.')
+        print('{0}. Too damn high. {1}.'.format(guess, name))
     elif guess == the_number:
-        print('Yay. You win goodbye.')
+        print('Yay {0]. You win goodbye.'.format(name))
     else:
         'WTF kinda guess is that?'
 
