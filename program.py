d4d47cb667ecc2a51185c87fc37f9c9e2508d395
badFarmer <guy@strategicmfg.com>
2019-01-02 18:40:08 -0800

    Initial Commit

--- program.py
+++ program.py
@@ -1,5 +1,9 @@
+#import random
+import random
+
 print('--------------------------------------------')
 print('              HELLO WORLD')
 print('--------------------------------------------')
 print()
 
+the_number = random.randint(0,100)
